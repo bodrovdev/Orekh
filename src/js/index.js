c73f4dd33b094349239f06831f1d90354a83c861
Igor Bodrov <bodrovigor02@gmail.com>
2022-11-03 17:57:46 +0300

    feat: 🎸 Обратная связь: разметка, стилизация, скрипты

--- src/js/index.js
+++ src/js/index.js
@@ -37,15 +37,52 @@ navLinks.forEach((element) => {
   })
 })
 
-function currentPage() {
-  let pageId = document.querySelector('.page-content').id;
+// function currentPage() {
+//   let pageId = document.querySelector('.page-content').id;
 
-  let navLinks = document.querySelectorAll('.main-nav__mobile-link');
-  navLinks.forEach((child) => {
-    if (child.dataset.name === pageId) {
-      child.classList.add('main-nav__mobile-link--active');
-    }
+//   let navLinks = document.querySelectorAll('.main-nav__mobile-link');
+//   navLinks.forEach((child) => {
+//     if (child.dataset.name === pageId) {
+//       child.classList.add('main-nav__mobile-link--active');
+//     }
+//   })
+// }
+
+// currentPage();
+
+//Выпадашка в форме на странице аренда и реклама
+let form_select = document.getElementById('form_1_select');
+let form_select_text = document.querySelector('.feedbackPage-tabs__select-text');
+let form_select_input = document.getElementById('form_1_select_input');
+
+let form_select_options = document.getElementById('form_1_select_options');
+let form_select_options_items = document.querySelectorAll('.feedbackPage-tabs__select-option');
+
+form_select.addEventListener('click', () => {
+  form_select_options.classList.toggle('feedbackPage-tabs__select-options--active');
+})
+
+form_select.addEventListener('keypress', function (event) {
+  if (document.activeElement === form_select && (event.key === "Enter" || event.key === "Space")) {
+    event.preventDefault();
+    form_select_options.classList.toggle('feedbackPage-tabs__select-options--active');
+  }
+});
+
+form_select_options_items.forEach((child) => {
+  child.addEventListener('click', () => {
+    form_select_text.textContent = child.dataset.value;
+    form_select_input.value = child.dataset.value;
+
+    form_select_options.classList.remove('feedbackPage-tabs__select-options--active');
   })
-}
+})
 
-currentPage();
+document.addEventListener('click', (event) => {
+  if (form_select_options.classList.contains('feedbackPage-tabs__select-options--active')
+    && event.target !== form_select_options
+    && event.target !== form_select_text
+    && event.target !== form_select) {
+    form_select_options.classList.remove('feedbackPage-tabs__select-options--active');
+  }
+})
