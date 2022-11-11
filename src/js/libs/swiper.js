53c9ac8f424e54cefb76ec7ba6ab39cf447b50bb
Igor Bodrov <bodrovigor02@gmail.com>
2022-11-11 17:54:56 +0300

    feat: 🎸 Страница новости: разметка, стилизация

--- src/js/libs/swiper.js
+++ src/js/libs/swiper.js
@@ -1,44 +1,16 @@
 import Swiper from 'swiper/bundle';
 
-// let floorsSwiper = Swiper;
-// let init = false;
+let news_slider_2 = new Swiper(".newsCatalogue-item__thumb-slider", {
+  spaceBetween: 10,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
 
-// function swiperMode() {
-//   let mobile = window.matchMedia('(min-width: 0px) and (max-width: 767px)');
-//   let tablet = window.matchMedia('(min-width: 768px) and (max-width: 1023px)');
-//   let desktop = window.matchMedia('(min-width: 1024px)');
-
-//   // Инициализация свайпера для мобильных устройств
-//   if (mobile.matches) {
-//     if (!init) {
-//       init = true;
-//       floorsSwiper = new Swiper('.inside__test-slider', {
-//         slidesPerView: 1,
-//         loop: true,
-//         spaceBetween: 10,
-//         direction: 'horizontal',
-//       });
-//     }
-//   }
-
-//   // Уничтожение слайдера на планшетах
-//   else if (tablet.matches) {
-//     floorsSwiper.destroy();
-//     init = false;
-//   }
-
-//   // Уничтожение слайдера на десктопе
-//   else if (desktop.matches) {
-//     floorsSwiper.destroy();
-//     init = false;
-//   }
-// }
-
-// window.addEventListener('load', function () {
-//   swiperMode();
-// });
-
-
-// window.addEventListener('resize', function () {
-//   swiperMode();
-// });
+let news_slider = new Swiper(".newsCatalogue-item__slider", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  thumbs: {
+    swiper: news_slider_2,
+  },
+});
